8bb75aef8fdcc2f487152efa6be404e79ecffaa9
RajulDixitNu10 <rajuld@nu10.co>
2022-09-08 20:29:44 +0000

    profile in progress

---
+++ src/app/account/profile/profile-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "prefix",
+    children: [
+      {
+        path: "profile",
+        component: ProfileComponent
+      },
+      {
+        path: "**",
+        redirectTo: "profile",
+        pathMatch: "full"
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
--- src/app/account/profile/profile.module.ts
+++ src/app/account/profile/profile.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    ProfileRoutingModule
   ]
 })
 export class ProfileModule { }
